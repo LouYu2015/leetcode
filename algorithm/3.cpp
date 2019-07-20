289433c85afcd12e0a50ed3ab0e5a0fe730f8844
Tom Lou <louyu27@cs.washington.edu>
2019-07-19 22:03:15 -0700

    Improve problem 3

--- algorithm/3.cpp
+++ algorithm/3.cpp
@@ -3,16 +3,17 @@ public:
     int lengthOfLongestSubstring(string s) {
         int a = 0;
         int n = s.length();
-        set<char> chars;
+        bool chars[256];
+        memset(chars, 0, sizeof(chars));
         int longest = 0;
         for (int i = 0; i < n; i++) {
-            if (chars.find(s[i]) != chars.end()) {
+            if (chars[s[i]]) {
                 do {
-                    chars.erase(s[a]);
+                    chars[s[a]] = false;
                     a++;
                 } while (s[a - 1] != s[i]);
             }
-            chars.insert(s[i]);
+            chars[s[i]] = true;
             longest = max(longest, i - a + 1);
         }
         return longest;
