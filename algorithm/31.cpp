5ffd29f60ba726f59c0d6e78df1ef55bd81f7f7d
Tom Lou <louyu27@cs.washington.edu>
2019-08-29 15:30:07 +0800

    p31(AC)

--- algorithm/31.cpp
+++ algorithm/31.cpp
@@ -1,16 +1,33 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
+        // Special case: empty or single element
+        // permutation won't change
         if (nums.size() <= 1) {
             return;
         }
-        int i = nums.size() - 2;
-        while (i >= 0 && nums[i] >= nums[i+1]) {
-            i--;
+        
+        int i;
+        for (i = nums.size() - 2; i >= 0; i--) {
+            // Find another number that can increase nums[i]
+            int min_index = -1;
+            for (int j = i + 1; j < nums.size(); j++) {
+                // The number should be greater than this number, but it shoud also be minimum so that the new permutation is adjacent to current one
+                if (nums[j] > nums[i] && (min_index == -1 || nums[j] < nums[min_index])) {
+                    min_index = j;
+                }
+            }
+            
+            // If such a number is found, increase this number
+            // else, continue to other position
+            if (min_index != -1) {
+                // Increase nums[i]
+                swap(nums[i], nums[min_index]);
+                break;
+            }
         }
-        if (i >= 0) {
-            swap(nums[i], nums[i+1]);
-        }
+        
+        // Other parts should be sorted so that it's a lexicorgraphically minimum permutation
         sort(nums.begin() + (i + 1), nums.end());
     }
 };
