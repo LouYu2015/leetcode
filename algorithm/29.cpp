6aa85c5b9312411ab3ced800538fd2036d596f00
Tom Lou <louyu27@cs.washington.edu>
2019-08-29 12:19:27 +0800

    p29(RE)

--- algorithm/29.cpp
+++ algorithm/29.cpp
@@ -1,27 +1,30 @@
 class Solution {
 public:
     int divide(int dividend, int divisor) {
-        if (dividend == -0x80000000 && divisor == -1) {
+        if (dividend == (~0x80000000+1) && divisor == -1) {
             return 0x7FFFFFFF;
         } else {
-            if (divisor < 0) {
-                divisor = -divisor;
-                dividend = -dividend;
-            }
+            bool negative = (dividend >= 0 && divisor < 0) || (dividend < 0 && divisor >= 0);
+            unsigned u_dividend = dividend > 0? dividend: -(unsigned)dividend;
+            unsigned u_divisor = divisor > 0? divisor: -(unsigned)divisor;
             
             int result = 0;
-            if (dividend >= 0) {
-                while (dividend >= divisor) {
-                    result++;
-                    dividend -= divisor;
-                }
-            } else {
-                while (dividend <= -divisor) {
-                    result--;
-                    dividend += divisor;
-                }
+            int current_bit = 1;
+            while (0x7FFFFFFF - u_divisor >= u_divisor && u_dividend >= u_divisor) {
+                current_bit <<= 1;
+                u_divisor <<= 1;
             }
-            return result;
+
+            while (current_bit > 0) {
+                if (u_dividend >= u_divisor) {
+                    result += current_bit;
+                    u_dividend -= u_divisor;
+                }
+                current_bit >>= 1;
+                u_divisor >>= 1;
+            }
+            return negative? -result: result;
         }
     }
 };
+
