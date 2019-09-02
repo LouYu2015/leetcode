fbf21fd2edaa78b42b87428b7655e9b72b701504
Tom Lou <louyu27@cs.washington.edu>
2019-09-02 10:50:54 +0800

    Find First and Last Position of Element in Sorted Array(TLE)

---
+++ algorithm/34.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+        int lowerBound = binarySearch(nums, target);
+        
+        if (nums[lowerBound] != target) {
+            vector<int> result;
+            result.push_back(-1);
+            result.push_back(-1);
+            return result;
+        } else {
+            int upperBound = binarySearch(nums, target + 1);
+            vector<int> result;
+            result.push_back(lowerBound);
+            result.push_back(upperBound);
+            return result;
+        }
+    }
+    
+    int binarySearch(vector<int>& nums, int target) {
+        int n = nums.size();
+        int low = 0, high = n, mid = (low + high) / 2;
+        // invariant: nums[low] < target <= nums[high]
+        while (high - low > 1) {
+            if (nums[mid] < target) {
+                low = mid;
+            } else {
+                high = mid;
+            }
+        }
+        return low + 1;
+    }
+};
