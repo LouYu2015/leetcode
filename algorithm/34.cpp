19ab28ebaa089324cd9077eac1fe3d1848d1f965
Tom Lou <louyu27@cs.washington.edu>
2019-09-02 10:59:48 +0800

    Find First and Last Position of Element in Sorted Array(AC)

--- algorithm/34.cpp
+++ algorithm/34.cpp
@@ -1,15 +1,20 @@
 class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
+        // Find lower bound of index
         int lowerBound = binarySearch(nums, target);
         
         if (lowerBound >= nums.size() || nums[lowerBound] != target) {
+            // Target not found
             vector<int> result;
             result.push_back(-1);
             result.push_back(-1);
             return result;
         } else {
+            // Find upper bound of index
             int upperBound = binarySearch(nums, target + 1) - 1;
+            
+            // Return
             vector<int> result;
             result.push_back(lowerBound);
             result.push_back(upperBound);
@@ -17,9 +22,10 @@ public:
         }
     }
     
+    // Binary search for first index where the number is greater or equal to target
     int binarySearch(vector<int>& nums, int target) {
         int n = nums.size();
-        int low = 0, high = n, mid = (low + high) / 2;
+        int low = -1, high = n, mid = (low + high) / 2;
         // invariant: nums[low] < target <= nums[high]
         while (high - low > 1) {
             if (nums[mid] < target) {
