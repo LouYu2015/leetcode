3d6b1eb1db05afb8a2508d6589cc55ed93147219
Tom Lou <louyu27@cs.washington.edu>
2019-09-03 12:34:34 +0800

    Combination Sum(AC)

--- algorithm/39.cpp
+++ algorithm/39.cpp
@@ -1,39 +1,26 @@
 class Solution {
 public:
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        vector<vector<int>> result;
-        set<set<int>> original_result = combinationSumHelper(candidates, target);
-        for (set<int> item : original_result) {
-            vector<int> result_item;
-            for (int num : item) {
-                result_item.push_back(num);
-            }
-            result.push_back(result_item);
-        }
-        return result;
-    }
-    
-    set<set<int>> combinationSumHelper(vector<int>& candidates, int target) {
         if (target < 0) {
-            return set<set<int>>();
+            return vector<vector<int>>();
         } else if (target == 0) {
-            set<set<int>> result;
-            result.insert(set<int>());
+            vector<vector<int>> result;
+            result.push_back(vector<int>());
             return result;
         } else if (candidates.size() == 0) {
-            return set<set<int>>();
+            return vector<vector<int>>();
         } else {
             int current = candidates.back();
             candidates.pop_back();
-            set<set<int>> result;
+            vector<vector<int>> result;
             
             for (int n = 0; target - current * n >= 0; n++) {
-                set<set<int>> sub_result = combinationSumHelper(candidates, target - n * current);
-                for (set<int> item : sub_result) {
+                vector<vector<int>> sub_result = combinationSum(candidates, target - n * current);
+                for (vector<int> item : sub_result) {
                     for (int k = 0; k < n; k++) {
-                        item.insert(current);
+                        item.push_back(current);
                     }
-                    result.insert(item);
+                    result.push_back(item);
                 }
             }
             candidates.push_back(current);
