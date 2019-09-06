42b61e44fbe74870a1a22ac6ed0f70af54d26713
Tom Lou <louyu27@cs.washington.edu>
2019-09-06 15:42:29 +0800

    Spiral Matrix(WA)

---
+++ algorithm/54.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+        vector<int> result;
+        
+        // Edge case: matrix is empty
+        if (matrix.size() == 0 || matrix[0].size() == 0) {
+            return result;
+        }
+        
+        int m = matrix.size();
+        int n = matrix[0].size();
+        
+        int upper = 0, bottom = m - 1, left = 0, right = n - 1;
+        while (upper <= bottom && left <= right) {
+            for (int i = left; i <= right; i++) {
+                result.push_back(matrix[upper][i]);
+            }
+            upper++;
+            if (upper > bottom)
+                break;
+            
+            for (int i = upper; i <= bottom; i++) {
+                result.push_back(matrix[i][right]);
+            }
+            right--;
+            if (left > right)
+                break;
+            
+            for (int i = right; i >= left; i--) {
+                result.push_back(matrix[bottom][i]);
+            }
+            bottom++;
+            if (upper > bottom)
+                break;
+            
+            for (int i = bottom; i >= upper; i--) {
+                result.push_back(matrix[i][left]);
+            }
+            left++;
+        }
+        return result;
+    }
+};
