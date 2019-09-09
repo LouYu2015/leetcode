b6c7659ca23ad4c6d0729730bff4bd03855d9a01
Tom Lou <louyu27@cs.washington.edu>
2019-09-09 21:09:58 +0800

    Permutation Sequence(RE)

---
+++ algorithm/60.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    string getPermutation(int n, int k) {
+        string result;
+        int p = 1;
+        for (int i = 2; i < n; i++) {
+            p *= i;
+        }
+        
+        vector<char> used(n, false);
+        for (int i = n - 1; i >= 0; i--) {
+            int j = k / p;
+            for (int num = 0; num < n; num++) {
+                if (!used[num]) {
+                    if (j == 0) {
+                        result += num + '0';
+                        used[num] = true;
+                    }
+                    j--;
+                }
+            }
+            k = k % p;
+            p /= i;
+        }
+        return result;
+    }
+};
