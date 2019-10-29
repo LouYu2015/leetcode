ae0c4ff6b210050bf952b5253d453ad542308dbb
Tom Lou <louyu27@cs.washington.edu>
2019-10-29 10:35:29 -0700

    Set Matrix Zeroes(WA)

--- algorithm/73.cpp
+++ algorithm/73.cpp
@@ -10,8 +10,8 @@ public:
             return;
         }
         
-        for (int x = 0; x < m; x++) {
-            for (int y = 0; y < n; y++) {
+        for (int x = 1; x < m; x++) {
+            for (int y = 1; y < n; y++) {
                 if (matrix[x][y] == 0) {
                     matrix[x][0] = 0;
                     matrix[0][y] = 0;
@@ -19,11 +19,33 @@ public:
             }
         }
         
-        for (int x = 0; x < m; x++) {
-            for (int y = 0; y < n; y++) {
+        for (int x = 1; x < m; x++) {
+            for (int y = 1; y < n; y++) {
                 if (matrix[x][0] == 0 || matrix[0][y] == 0) {
                     matrix[x][y] = 0;
                 }
+            }
+        }
+        bool first_row_zero = false;
+        bool first_col_zero = false;
+        for (int x = 0; x < m; x++) {
+            if (matrix[x][0] == 0) {
+                first_col_zero = true;
+            }
+        }
+        for (int y = 0; y < n; y++) {
+            if (matrix[0][y] == 0) {
+                first_row_zero = true;
+            }
+        }
+        if (first_col_zero) {
+            for (int x = 0; x < m; x++) {
+                matrix[x][0] = 0;
+            }
+        }
+        if (first_row_zero) {
+            for (int y = 0; y < n; y++) {
+                matrix[0][y] = 0;
             }
         }
     }
