dea0a19bf8046fdf468aeeaa00e8c85d742fb7b6
Tom Lou <louyu27@cs.washington.edu>
2019-10-29 09:50:19 -0700

    Simplify Path(RTE)

---
+++ algorithm/71.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    int find_next(string &path, int i) {
+        while (i < path.length() && path[i] != '/') {
+            i++;
+        }
+        return i;
+    }
+    
+    string simplifyPath(string path) {
+        int current = 0;
+        vector<string> names;
+        while (current < path.length()) {
+            if (path[current] == '/') {
+                current++;
+            }
+            if (current >= path.length()) {
+                break;
+            }
+            int end = find_next(path, current);
+            string name = path.substr(current, end - current);
+            if (name == "" || name == ".") {
+                continue;
+            }
+            if (name == "..") {
+                names.pop_back();
+            } else {
+                names.push_back(name);
+            }
+            current = end;
+        }
+        string result;
+        if (path.length() > 0 && path[0] == '/') {
+            result += '/';
+        }
+        for (int i = 0; i < names.size(); i++) {
+            result += names[i];
+            if (i != names.size() - 1) {
+                result += '/';
+            }
+        }
+        return result;
+    }
+};
