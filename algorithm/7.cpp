9dacbec628ec409170d378d3a4888b4358ad28fe
Tom Lou <louyu27@cs.washington.edu>
2019-10-29 11:09:58 -0700

    Reverse Integer(AC)

--- algorithm/7.cpp
+++ algorithm/7.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
     int reverse(int x) {
-        int result;
+        int result = 0;
         bool is_negative = x < 0;
+        if (x == INT_MIN) {
+            return 0;
+        }
         if (x < 0) {
             x = -x;
         }
         while (x != 0) {
+            if (result > (INT_MAX - x%10)/10) {
+                return 0;
+            }
             result *= 10;
-            if (result < 0) {
-                return 0;
-            }
             result += x % 10;
-            if (result < 0) {
-                return 0;
-            }
             x /= 10;
         }
         if (result == INT_MIN) {
