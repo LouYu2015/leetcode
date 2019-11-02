581808de8426528f6662ea74a63485c144034956
Tom Lou <louyu27@cs.washington.edu>
2019-11-02 11:18:36 -0700

    Valid Parentheses(CE)

---
+++ algorithm/20.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        vector<char> stack;
+        hash_map<char, char> bracket_pair;
+        bracket_pair[')'] = '(';
+        bracket_pair['}'] = '{';
+        bracket_pair[']'] = '[';
+        for (char c : s) {
+            if (c == '(' || c == '{' || c == '[') {
+                stack.push_back(c);
+            } else {
+                if (stack.size() == 0 || stack.back() != bracket_pair[c]) {
+                    return false;
+                }
+                stack.pop_back();
+            }
+        }
+        return true;
+    }
+};
