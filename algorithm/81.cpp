7ceaa6f598345cf160379a728059627765e611d3
Tom Lou <louyu27@cs.washington.edu>
2019-11-10 09:19:12 -0800

    Search in Rotated Sorted Array II(WA)

---
+++ algorithm/81.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    bool search(vector<int>& nums, int target) {
+        int n = nums.size();
+        int low = find_start(nums, 0, n - 1);
+        if (low == -1) {
+            low = 0;
+        }
+        int high = low + n;
+        int mid;
+        while (high - low > 1) {
+            mid = (low + high) / 2;
+            if (nums[mid % n] <= target) {
+                low = mid;
+            } else {
+                high = mid;
+            }
+        }
+        return nums[low % n] == target;
+    }
+    
+    int find_start(vector<int> &nums, int low, int high) {
+        if (high - low == 0) {
+            return -1;
+        } else if (high - low == 1) {
+            return nums[high] < nums[low]? high : -1;
+        }
+        int mid = (low + high) / 2;
+        int left_result = -1;
+        int right_result = -1;
+        if (nums[high] <= nums[mid]) {
+            left_result = find_start(nums, mid, high);
+        }
+        if (nums[mid] <= nums[low]) {
+            right_result = find_start(nums, low, mid);
+        }
+        return left_result == -1? right_result : left_result;
+    }
+};
