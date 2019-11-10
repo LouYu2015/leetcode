41cd9d4ff1363e415a0bc71db23af4e1dac5d1e6
Tom Lou <louyu27@cs.washington.edu>
2019-11-09 16:18:02 -0800

    Maximum Subarray(AC)

--- algorithm/53.cpp
+++ algorithm/53.cpp
@@ -1,14 +1,16 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
+        assert(nums.size() > 0);
+        
         int current_sum = 0;
         int min_sum = 0;
-        int result = 0;
+        int result = nums[0];
         
         for (int num : nums) {
             current_sum += num;
-            min_sum = min(min_sum, current_sum);
             result = max(result, current_sum - min_sum);
+            min_sum = min(min_sum, current_sum);
         }
         
         return result;
