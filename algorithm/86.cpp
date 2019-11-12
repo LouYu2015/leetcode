c85dc47b261212fcf86b7ba809f1fe61624fb6cd
Tom Lou <louyu27@cs.washington.edu>
2019-11-12 11:49:53 -0800

    Partition List(TLE)

---
+++ algorithm/86.cpp
@@ -0,0 +1,39 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* partition(ListNode* head, int x) {
+        ListNode *l1_head = nullptr;
+        ListNode *l1 = nullptr;
+        ListNode *l2_head = nullptr;
+        ListNode *l2 = nullptr;
+        while (head != nullptr) {
+            if (head->val < x) {
+                if (l1 == nullptr) {
+                    l1_head = l1 = head;
+                } else {
+                    l1->next = head;
+                    l1 = head;
+                }
+            } else {
+                if (l2 == nullptr) {
+                    l2_head = l2 = head;
+                } else {
+                    l2->next = head;
+                    l2 = head;
+                }
+            }
+            head = head->next;
+        }
+        if (l1 != nullptr) {
+            l1->next = l2_head;
+        }
+        return l1_head == nullptr? l2_head : l1_head;
+    }
+};
