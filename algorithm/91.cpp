916132ac9b1f44ac5955d7bd55e4e1e26bb9d713
Tom Lou <louyu27@cs.washington.edu>
2019-11-17 12:17:00 -0800

    Decode Ways(WA)

---
+++ algorithm/91.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int numDecodings(string s) {
+        vector<int> ways(s.length() + 2, 0); // ways[i] = Number of ways to decode the string starting from index i
+        for (int i = s.length() - 1; i >= 0; i--) {
+            ways[i] = ways[i + 1];
+            if (i + 1 < s.length()) {
+                int num = (s[i] - '0') * 10 + s[i + 1] - '0';
+                if (num <= 26) {
+                    ways[i] += ways[i + 2];
+                }
+            }
+        }
+        return ways[0];
+    }
+};
