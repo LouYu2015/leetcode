fa5ccd6dfc863f0edba33245deddd228715da35a
Tom Lou <louyu27@cs.washington.edu>
2019-11-23 09:40:57 -0800

    Merge Sorted Array(WA)

---
+++ algorithm/88.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int k = m + n - 1;
+        m--;
+        n--;
+        // invariant: nums1[k..m+n-1] is merged and sorted array
+        //   nums1[0..m] is not merged and nums2[0..n] is not merged
+        while (k > 0) {
+            if (m == -1 || (m != -1 && n != -1 && nums2[n] > nums1[m])) {
+                nums1[k] = nums2[n];
+                n--;
+            } else {
+                nums1[k] = nums1[m];
+                m--;
+            }
+            k--;
+        }
+    }
+};
