82948e81abfc40ca0276c0a837432124dbd8f3b1
Tom Lou <louyu27@cs.washington.edu>
2019-11-23 09:42:45 -0800

    Merge Sorted Array(AC)

--- algorithm/88.cpp
+++ algorithm/88.cpp
@@ -4,9 +4,9 @@ public:
         int k = m + n - 1;
         m--;
         n--;
-        // invariant: nums1[k..m+n-1] is merged and sorted array
+        // invariant: nums1[k+1..m+n-1] is merged and sorted array
         //   nums1[0..m] is not merged and nums2[0..n] is not merged
-        while (k > 0) {
+        while (k >= 0) {
             if (m == -1 || (m != -1 && n != -1 && nums2[n] > nums1[m])) {
                 nums1[k] = nums2[n];
                 n--;
