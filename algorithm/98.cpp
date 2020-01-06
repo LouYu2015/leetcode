538b48e510e291d4a9c3687765ee247a5f00c460
Tom Lou <louyu27@cs.washington.edu>
2020-01-05 23:01:56 -0800

    Validate Binary Search Tree(AC)

--- algorithm/98.cpp
+++ algorithm/98.cpp
@@ -10,22 +10,17 @@
 class Solution {
 public:
     bool isValidBST(TreeNode* root) {
-        return isValidBST(root, INT_MIN, INT_MAX);
+        return isValidBST(root, INT_MIN, INT_MAX, true, true);
     }
     
-    bool isValidBST(TreeNode *root, int min, int max) {
+    // left_inf: true if the minimum number represents infinity. Used to check against INT_MIN test case.
+    bool isValidBST(TreeNode *root, int min, int max, bool left_inf, bool right_inf) {
         if (root == nullptr) {
             return true;
         }
-        if (root->val <= min || root->val >= max) {
+        if ((!left_inf && root->val <= min) || (!right_inf && root->val >= max)) {
             return false;
         }
-        if (root->left && !isValidBST(root->left, min, root->val)) {
-            return false;
-        }
-        if (root->right && !isValidBST(root->right, root->val, max)) {
-            return false;
-        }
-        return true;
+        return isValidBST(root->left, min, root->val, left_inf, false) && isValidBST(root->right, root->val, max, false, right_inf);
     }
 };
