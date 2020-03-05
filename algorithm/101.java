e6c81e8d0780cee9172ab4aedb23a0cad4886bcf
Tom Lou <louyu27@cs.washington.edu>
2020-03-04 21:31:30 -0800

    101

---
+++ algorithm/101.java
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        
+        Queue<TreeNode> left = new ArrayDequeue<>();
+        Queue<TreeNode> right = new ArrayDequeue<>();
+        
+        left.offer(root.left);
+        right.offer(root.right);
+        
+        while (left.size() > 0) {
+            TreeNode l = left.poll();
+            TreeNode r = right.poll();
+            
+            if (l == null && r == null) {
+                continue;
+            } else if (l == null || r == null) {
+                return false;
+            } else if (l.val != r.val) {
+                return false;
+            }
+            
+            left.offer(l.right);
+            right.offer(r.left);
+            
+            left.offer(l.left);
+            right.offer(r.right);
+        }
+        return true;
+    }
+}
