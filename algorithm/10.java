c393155a45c5993faff7ed721f59974ee2ffc801
Tom Lou <louyu27@cs.washington.edu>
2020-03-15 22:49:37 -0700

    10 (CE)

---
+++ algorithm/10.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean isMatch(String s, String p) {
+        return isMatch(s, 0, p, 0);
+    }
+    
+    public boolean isMatch(String s, int sc, String p, int pc) {
+        if (sc == s.length && pc == p.length) {
+            return true;
+        } else if (sc >= s.length || pc >= p.legth) {
+            return false;
+        }
+        
+        if (s.charAt(sc) == '.') {
+            return isMatch(s, sc + 1, p, pc + 1);
+        }
+        
+        boolean result = false;
+        if (sc + 1 < s.length && s.charAt(sc + 1) == '*') {
+            if (s.charAt(sc) == p.charAt(pc)) {
+                result = result || isMatch(s, sc, p, pc + 1);
+            }
+            result = result || isMatch(s, sc + 2, p, pc);
+        } else {
+            result = s.charAt(sc) == p.charAt(pc) && isMatch(s, sc + 1, p, pc + 1);
+        }
+        return result;
+    }
+}
