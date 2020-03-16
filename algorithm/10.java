fdffb54d8883873f82e92cceb47b71dd3be26a5b
Tom Lou <louyu27@cs.washington.edu>
2020-03-15 23:27:36 -0700

    10 (WA)

--- algorithm/10.java
+++ algorithm/10.java
@@ -5,23 +5,28 @@ class Solution {
     
     public boolean isMatch(String s, int sc, String p, int pc) {
         if (sc == s.length() && pc == p.length()) {
+            System.out.println("8:" + sc + " " + pc + " " + "true");
             return true;
         } else if (sc >= s.length() || pc >= p.length()) {
+            System.out.println("11:" + sc + " " + pc + " " + "false");
             return false;
         }
         
-        if (s.charAt(sc) == '.') {
+        if (p.charAt(pc) == '.') {
             return isMatch(s, sc + 1, p, pc + 1);
         }
         
         boolean result = false;
-        if (sc + 1 < s.length() && s.charAt(sc + 1) == '*') {
+        if (pc + 1 < p.length() && p.charAt(pc + 1) == '*') {
             if (s.charAt(sc) == p.charAt(pc)) {
-                result = result || isMatch(s, sc, p, pc + 1);
+                result = result || isMatch(s, sc + 1, p, pc);
+                System.out.println("23:" + sc + " " + pc + " " + result);
             }
-            result = result || isMatch(s, sc + 2, p, pc);
+            result = result || isMatch(s, sc, p, pc + 2);
+            System.out.println("26:" + sc + " " + pc + " " + result);
         } else {
             result = s.charAt(sc) == p.charAt(pc) && isMatch(s, sc + 1, p, pc + 1);
+            System.out.println("29:" + sc + " " + pc + " " + result);
         }
         return result;
     }
