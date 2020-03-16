35efd69cadb62447476db789c0aacf8c05ef12e1
Tom Lou <louyu27@cs.washington.edu>
2020-03-15 22:23:47 -0700

    38 (CE)

---
+++ algorithm/38.java
@@ -0,0 +1,32 @@
+class Solution {
+    public String countAndSay(int n) {
+        assert n >= 1;
+        
+        // Base case
+        if (n == 1) {
+            return "1";
+        }
+        
+        String prevResult = countAndSay(n - 1);
+        char prevChar = '\n';
+        int count = 0;
+        StringBuffer result = new StringBuffer();
+        for (char c : prevResult) {
+            if (c == prevChar) {
+                count++;
+            } else {
+                if (count != 0) {
+                    result.append(count);
+                    result.append(prevChar);
+                }
+                count = 1;
+            }
+            prevChar = c;
+        }
+        
+        result.append(count);
+        result.append(prevChar);
+        
+        return result.toString();
+    }
+}
